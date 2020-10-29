ba6e75cff7bb3314d170f24c0b0b9f699f0d88bc
akitama3368 <62548266+akitama3368@users.noreply.github.com>
2020-10-29 22:17:28 +0900

    Update download.py

--- download.py
+++ download.py
@@ -1,4 +1,4 @@
 import urllib
 url = 'https://github.com/jagrosh/MusicBot/releases/download/0.3.1/JMusicBot-0.3.1.jar'
 title = 'JMusicBot.jar'
-urllib.urlretrieve(url, title)
+urllib.request.urlretrieve(url, title)
