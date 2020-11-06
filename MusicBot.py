526c28e0c41bde4f3ce8f9ac0ec6c907799bfbea
akitama3368 <62548266+akitama3368@users.noreply.github.com>
2020-11-06 18:52:30 +0900

    Update MusicBot.py

--- MusicBot.py
+++ MusicBot.py
@@ -64,7 +64,7 @@ class YTDLSource(discord.PCMVolumeTransformer):
 
     FFMPEG_OPTIONS = {
         'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
-        'options': '-vn -codec:a libfdk_aac -vbr 5',
+        'options': '-vn -codec:a aac -vbr 5',
     }
 
     ytdl = youtube_dl.YoutubeDL(YTDL_OPTIONS)
