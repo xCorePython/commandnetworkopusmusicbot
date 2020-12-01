8e63fe9dd4a40d62ab1b3ab013c291f97fe13b52
akitama3368 <62548266+akitama3368@users.noreply.github.com>
2020-12-01 16:16:29 +0900

    Update MusicBot.py

--- MusicBot.py
+++ MusicBot.py
@@ -319,14 +319,17 @@ def search(value):
 
 async def np():
 	while sys_loop == 1:
-		data = q.np1()[0]
-		start = q.np2()
-		nptime = float(now_date('off', 9) - float(start))
-		duration = float(data['format']['duration'])
-		if nptime > duration:
-			nptime = duration
-		await client.get_channel(782863961153339403).edit(topic='Time : {} / {}\nTitle : {}\nUploader : {}\nCodec : {}\nBitrate : {}kbps / {}'.format(str(reverse(nptime)), str(reverse(duration)), data['title'], data['uploader'], data['streams'][0]['codec_long_name'], str(int(data['format']['bit_rate'])/1000), data['streams'][0]['channel_layout']))
-		await asyncio.sleep(4)
+		try:
+			data = q.np1()[0]
+			start = q.np2()
+			nptime = float(now_date('off', 9) - float(start))
+			duration = float(data['format']['duration'])
+			if nptime > duration:
+				nptime = duration
+			await client.get_channel(782863961153339403).edit(topic='Time : {} / {}\nTitle : {}\nUploader : {}\nCodec : {}\nBitrate : {}kbps / {}'.format(str(reverse(nptime)), str(reverse(duration)), data['title'], data['uploader'], data['streams'][0]['codec_long_name'], str(int(data['format']['bit_rate'])/1000), data['streams'][0]['channel_layout']))
+			await asyncio.sleep(4)
+		except:
+			await asyncio.sleep(10)
 
 def conv(info_dict):
 	title = info_dict['id'] + '.mp3'
