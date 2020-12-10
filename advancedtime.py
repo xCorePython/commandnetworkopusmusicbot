7636459c939abc3dd1450bfaaeb643aeaadef9e8
akitama3368 <62548266+akitama3368@users.noreply.github.com>
2020-12-10 19:12:12 +0900

    Update advancedtime.py

--- advancedtime.py
+++ advancedtime.py
@@ -1,86 +1,85 @@
 import datetime, calendar
 
-class advancedtime:
-	def checkmonth(self, mode):
-		if mode == 'total':
-			now = datetime.datetime.utcnow()
-			if now.month == 1:
-				a01 = 0
-				for n in range(1):
-					nowcalendar = str(calendar.month(int('{}'.format(now.year)), int('{}'.format(n))))
-					a01 = a01 + int(nowcalendar[int(len(nowcalendar) -3):int(len(nowcalendar) - 1)])
-				a01 = a01 + now.day
-				return a01
-			if now.month > 2:
-				a01 = 0
-				for n in range(1, now.month):
-					nowcalendar = str(calendar.month(int('{}'.format(now.year)), int('{}'.format(n))))
-					a01 = a01 + int(nowcalendar[int(len(nowcalendar) -3):int(len(nowcalendar) - 1)])
-				a01 = a01 + now.day
-				return a01
-		if mode == 'month':
-			now = datetime.datetime.utcnow()
-			nowcalendar = str(calendar.month(int('{}'.format(now.year)), int('{}'.format(now.month))))
-			a01 = int(nowcalendar[int(len(nowcalendar) - 3):int(len(nowcalendar) - 1)])
+def checkmonth(mode):
+	if mode == 'total':
+		now = datetime.datetime.utcnow()
+		if now.month == 1:
+			a01 = 0
+			for n in range(1):
+				nowcalendar = str(calendar.month(int('{}'.format(now.year)), int('{}'.format(n))))
+				a01 = a01 + int(nowcalendar[int(len(nowcalendar) -3):int(len(nowcalendar) - 1)])
+			a01 = a01 + now.day
 			return a01
+		if now.month > 2:
+			a01 = 0
+			for n in range(1, now.month):
+				nowcalendar = str(calendar.month(int('{}'.format(now.year)), int('{}'.format(n))))
+				a01 = a01 + int(nowcalendar[int(len(nowcalendar) -3):int(len(nowcalendar) - 1)])
+			a01 = a01 + now.day
+			return a01
+	if mode == 'month':
+		now = datetime.datetime.utcnow()
+		nowcalendar = str(calendar.month(int('{}'.format(now.year)), int('{}'.format(now.month))))
+		a01 = int(nowcalendar[int(len(nowcalendar) - 3):int(len(nowcalendar) - 1)])
+		return a01
 
-	def checktime(self, mode, location):
-		if mode == 'off':
-			now = datetime.datetime.utcnow()
-			return float(now.strftime("0.%f")) + int(now.second) + int(int(int(now.month * 365) + int(self.checkmonth('month'))) * 86400) + int(int(now.day) * 86400) + int(int(now.hour) * 3600) + int(int(now.minute) * 60)
-		if mode == 'on':
-			now = datetime.datetime.utcnow()
-			locationtime = location
-			year = now.year
-			hour = now.hour + locationtime
-			day = now.day
-			month = now.month
-			if hour > 24:
-				hour2 = hour / 24
-				hour = hour - int(hour2 * 24)
-				day = day + 1
-				if day > self.checkmonth('month'):
-					month = month + 1
-					if month > 12:
-						month = month - 12
-						year = year + 1
-			a01 = datetime.datetime(year, month, day, hour, now.minute, now.second, int(now.strftime("%f")))
-			return a01.strftime("%Y/%m/%d %H:%M:%S.%f")
+def checktime(mode, location):
+	if mode == 'off':
+		now = datetime.datetime.utcnow()
+		return float(now.strftime("0.%f")) + int(now.second) + int(int(int(now.month * 365) + int(self.checkmonth('month'))) * 86400) + int(int(now.day) * 86400) + int(int(now.hour) * 3600) + int(int(now.minute) * 60)
+	if mode == 'on':
+		now = datetime.datetime.utcnow()
+		locationtime = location
+		year = now.year
+		hour = now.hour + locationtime
+		day = now.day
+		month = now.month
+		if hour > 24:
+			hour2 = hour / 24
+			hour = hour - int(hour2 * 24)
+			day = day + 1
+			if day > self.checkmonth('month'):
+				month = month + 1
+				if month > 12:
+					month = month - 12
+					year = year + 1
+		a01 = datetime.datetime(year, month, day, hour, now.minute, now.second, int(now.strftime("%f")))
+		return a01.strftime("%Y/%m/%d %H:%M:%S.%f")
 
-	def fetchtime(self, data):
-		time = int(float(data))
-		if time < 10:
-			second = int(time)
-			uptime = '0:0' + str(second)
-			return uptime
-		if time >= 60:
-			if time < 3600:
-				minute = int(time / 60)
-				second = int(time - minute * 60)
+def fetchtime(data):
+	time = int(float(data))
+	if time < 10:
+		second = int(time)
+		uptime = '0:0' + str(second)
+		return uptime
+	if time >= 60:
+		if time < 3600:
+			minute = int(time / 60)
+			second = int(time - minute * 60)
+			if second < 10:
+				uptime = str(minute) + ':0' + str(second)
+				return uptime
+			else:
+				uptime = str(minute) + ':' + str(second)
+				return uptime
+		else:
+			hour = int(time / 3600)
+			minute = int(int(time - hour * 3600) / 60)
+			second = int(time - hour * 3600 - minute * 60)
+			if minute < 10:
 				if second < 10:
-					uptime = str(minute) + ':0' + str(second)
+					uptime = str(hour) + ':0' + str(minute) + ':0' + str(second)
 					return uptime
 				else:
-					uptime = str(minute) + ':' + str(second)
+					uptime = str(hour) + ':0' + str(minute) + ':' + str(second)
 					return uptime
 			else:
-				hour = int(time / 3600)
-				minute = int(int(time - hour * 3600) / 60)
-				second = int(time - hour * 3600 - minute * 60)
-				if minute < 10:
-					if second < 10:
-						uptime = str(hour) + ':0' + str(minute) + ':0' + str(second)
-						return uptime
-					else:
-						uptime = str(hour) + ':0' + str(minute) + ':' + str(second)
-						return uptime
+				if second < 10:
+					uptime = str(hour) + ':' + str(minute) + ':0' + str(second)
+					return uptime
 				else:
-					if second < 10:
-						uptime = str(hour) + ':' + str(minute) + ':0' + str(second)
-						return uptime
-					else:
-						uptime = str(hour) + ':' + str(minute) + ':' + str(second)
-						return uptime
-		else:
-			uptime = '0:' + str(time)
-			return uptime
+					uptime = str(hour) + ':' + str(minute) + ':' + str(second)
+					return uptime
+	else:
+		uptime = '0:' + str(time)
+		return uptime
